0c17c5644147422c2d218628bfa4a69d0e79ec1b
Bret Combast <keldenar@gmail.com>
2015-06-24 08:48:22 -0400

    Updated adsense blade to support APP_ENV settings.

--- resources/views/adsense.blade.php
+++ resources/views/adsense.blade.php
@@ -10,13 +10,15 @@
 </div> -->
 
 <div class="col-md-2 text-center">
-    <!--
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <ins class="adsbygoogle" style="display:inline-block;width:300px;height:600px" data-ad-client="ca-pub-6849314491293990" data-ad-slot="3757761982"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-    -->
+    @if(Config::get("APP_ENV") == "production")
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins class="adsbygoogle" style="display:inline-block;width:300px;height:600px" data-ad-client="ca-pub-6849314491293990" data-ad-slot="3757761982"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    @else
+        <div class="outlined" style="width: 300px; height: 600px">This is a dummy add.</div>
+    @endif
 </div>
 
 
