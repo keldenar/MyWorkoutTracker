ad7f6574aa515df93b839cf0ccd98441508b60bf
Bret Combast <keldenar@gmail.com>
2015-06-24 08:55:39 -0400

    Updated adsense changed to use app.debug instead.

--- resources/views/adsense.blade.php
+++ resources/views/adsense.blade.php
@@ -10,7 +10,7 @@
 </div> -->
 
 <div class="col-md-2 text-center">
-    @if(Config::get("APP_DEBUG") == "false")
+    @if(Config::get("app.debug") == "false")
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <ins class="adsbygoogle" style="display:inline-block;width:300px;height:600px" data-ad-client="ca-pub-6849314491293990" data-ad-slot="3757761982"></ins>
         <script>
