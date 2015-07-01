1d346ba568e47af63d89d839545ff6555d450811
Bret Combast <keldenar@gmail.com>
2015-07-01 10:45:38 -0400

    Moved css and fonts to end of page.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,11 +6,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Exercise Tracker</title>
 
-    <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/workout.css') }}" rel="stylesheet">
-
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -53,6 +48,9 @@
     <script src="/js/moment/moment.js"></script>
     <script src="/js/workout.js"></script>
     <script src="/js/bootstrap.js"></script>
+    <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/workout.css') }}" rel="stylesheet">
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
     <script type="text/javascript" src="/js/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
     @if (Config::get("app.debug") == false)
